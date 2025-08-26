6090e5cfd26a08d8ee733570ef7efa86774c26ab
Ishan Saini <ishansaini0105@gmail.com>
2025-08-26 17:37:53 +0530

    The Projects are added to the website

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "ishansaini0105@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
